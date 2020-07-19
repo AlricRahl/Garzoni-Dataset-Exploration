6d92d4c50334a3e06ee3443b8adf4228f966b6e6
Baran Iscanli <barantevitol@gmail.com>
2020-07-19 21:24:21 +0300

    Added the mapping function for map of Europe

--- src/mapping_tool.py
+++ src/mapping_tool.py
@@ -9,11 +9,11 @@ import cleantools as clnt
 
 
 def plotMap(
-    df, column="Count", empty_color="lightgrey", map_color_scale="Greens"
+    df, figsize, column="Count", empty_color="lightgrey", map_color_scale="Greens"
 ):
     """Plots the given column in a data frame on italy map"""
     # Assign objects for control
-    fig, ax = plt.subplots(1, figsize=(10, 12), facecolor="lightblue")
+    fig, ax = plt.subplots(1, figsize=figsize, facecolor="lightblue")
 
     # Plot the values on empty map
     ax.set_title(
@@ -49,22 +49,32 @@ def filterNmap(
     column="Count",
     empty_color="lightgrey",
     map_color_scale="Greens",
+    figsize=(10,12),
+    eu=False,
     **kwargs
 ):
     """Takes in a dataframe, a dictionary of columns and their desired values,
     plots the number of apprentices in provinces on an Italy map, using the
     data acquired by filtering the data frame accordingly"""
-    gdf = gpd.read_file("../data/italy_city_map.shp")
+    if eu:
+        gdf = gpd.read_file("../data/europe_map.shp")
+        column_on_left = "Country"
+        column_for_map = "NAME_ENGL"
+    else:
+        gdf = gpd.read_file("../data/italy_city_map.shp")
+        column_on_left = "Apprentice Province"
+        column_for_map = "NOME_PRO"
     df = clnt.filterFrame(df, filterParam, s_year=s_year, **kwargs)
     values = pd.DataFrame(
-        df["Apprentice Province"].value_counts().apply(np.log),
-        columns=["Apprentice Province"],
+        df[column_on_left].value_counts().apply(np.log),
+        columns=[column_on_left],
     )
     values = values.reset_index()
-    values.columns = ["Province", column]
-    df = gdf.merge(values, left_on="NOME_PRO", right_on="Province", how="left")
+    values.columns = [column_on_left, column]
+    df = gdf.merge(values, left_on=column_for_map, right_on=column_on_left, how="left")
     plotMap(
         df,
+        figsize,
         column=column,
         empty_color=empty_color,
         map_color_scale=map_color_scale,
